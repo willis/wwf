6597c9aa7e30fb655d7e4df9cf9e29154af82053
tozhangwj <tozhangwj@5e0e2e99-4509-a61d-e072-a9001a804012>
2011-09-19 07:05:23 +0000

    工具类

--- system-admin/src/java/bot/com/mpaike/util/bot/UrlIO.java
+++ system-admin/src/java/bot/com/mpaike/util/bot/UrlIO.java
@@ -1,28 +1,75 @@
 package com.mpaike.util.bot;
 
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
 
 import com.mpaike.core.util.data.FastByteArrayOutputStream;
-import com.mpaike.core.util.resource.FileUtil;
-import com.mpaike.image.model.Picture;
-import com.mpaike.util.ExifHelper;
 
 public class UrlIO {
 	
 	public final static boolean DEBUG = true; //调试用
 	private static int BUFFER_SIZE = 8096; //缓冲区大小
+	
+	/**
+     * 将HTTP资源另存为文件
+     *
+     * @param destUrl String
+     * @param fileName String
+     * @throws Exception
+     */
+    public static String webAccess(String destUrl) {
+        BufferedReader br = null;
+        HttpURLConnection httpUrl = null;
+        URL url = null;
+        StringBuilder sb = new StringBuilder();
+        //建立链接
+        try {
+			url = new URL(destUrl);
+
+        httpUrl = (HttpURLConnection) url.openConnection();
+        //连接指定的资源
+        httpUrl.connect();
+        //获取网络输入流
+        br = new BufferedReader(new InputStreamReader(httpUrl.getInputStream()));
+        //检测
+        
+        String line; 
+        while((line = br.readLine())!=null){ 
+        		sb.append(line);
+        }
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(br!=null){
+				try {
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(httpUrl!=null){
+				httpUrl.disconnect();
+			}
+		}
+        return sb.toString();
+    }
 	
 	/**
      * 将HTTP资源另存为文件
@@ -170,5 +217,10 @@ public class UrlIO {
         System.getProperties().put("proxyHost", proxy);
         System.getProperties().put("proxyPort", proxyPort);
     }
+    
+    public static void main(String[] argv) throws IOException{
+    		System.out.print(webAccess("http://www.sina.com.cn"));
+    }
+    
 
 }
