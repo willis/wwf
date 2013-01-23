4fa6e613c2a2054757333728f21f65125778b5d6
tozhangwj <tozhangwj@5e0e2e99-4509-a61d-e072-a9001a804012>
2013-01-23 08:52:48 +0000

    SpringDaoImpl.java

---
+++ system-core/src/java/com/mpaike/core/database/hibernate/SpringDaoImpl.java
@@ -0,0 +1,10 @@
+package com.mpaike.core.database.hibernate;
+
+import java.io.Serializable;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class SpringDaoImpl<T extends Serializable> extends HibernateDaoSupport{
+		
+
+}
